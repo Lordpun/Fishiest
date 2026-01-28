3b174046d21f0c40464b0b7744b4933345d70b3c
Lucas Hartzell <hartzelllucas1@gmail.com>
2026-01-27 19:31:12 -0500

    Another attempt to fix site not appearing

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Fishiest/',
   plugins: [
     vue(),
     vueDevTools(),
